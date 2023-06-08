1164c1b75ebd9d652a679ab7d3dfa1e10c50be24
weidong <weidong.du@foxmail.com>
2023-06-08 10:35:47 +0800

    add bn

--- eastm/src/main/java/pro/jiaoyi/eastm/job/JobAlert.java
+++ eastm/src/main/java/pro/jiaoyi/eastm/job/JobAlert.java
@@ -93,9 +93,10 @@ public class JobAlert {
                     String fenshiAmtStr = amtStr(fenshiAmt);
                     EmDailyK k = dailyKs.get(dailyKs.size() - 1);
                     log.info("价格突破成功 code={} name={} 分时量{}", code, name, amtStr);
-                    String content = code + "_" + name + "_" + k.getClose() + "_" + k.getPct()
-                            + "<br>" + amtStr + ",M1=" + fx + "_" + fenshiAmtStr
-                            + "<br>" + LocalDateTime.now().toString().substring(0, 16) + k.getBk();
+                    String content = code + "_" + name + "_" + k.getBk()
+                            + "<br>" + "价格=" + k.getClose() + ",涨幅=" + k.getPct()
+                            + "<br>" + "标准量=" + amtStr + ",M1=" + fx + "_" + fenshiAmtStr
+                            + "<br>" + LocalDateTime.now().toString().substring(0, 16);
                     wxUtil.send(content);
 
 
