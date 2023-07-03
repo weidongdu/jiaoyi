0555d1e6bd75d5a470802b7578593e714c749a30
weidong <weidong.du@foxmail.com>
2023-07-03 10:49:16 +0800

    update bixuan

--- tradingview/src/main/java/pro/jiaoyi/tradingview/TradingviewApplication.java
+++ tradingview/src/main/java/pro/jiaoyi/tradingview/TradingviewApplication.java
@@ -24,11 +24,14 @@ public class TradingviewApplication {
             System.out.println(EmojiUtil.UP + "买入: 高开再新高,量要同步新高");
             System.out.println(EmojiUtil.UP + "买入: 仓位要控制,不要贪多");
 
-            System.out.println(EmojiUtil.WARN + "选出之后: 1,看大盘, 打分 1-10");
-            System.out.println(EmojiUtil.WARN + "选出之后: 2,看所属板块, 打分 1-10");
-            System.out.println(EmojiUtil.WARN + "选出之后: 3,看关联个股, 打分 1-10");
-            System.out.println(EmojiUtil.WARN + "选出之后: 4,看个股公告, 打分 1-10");
-            System.out.println(EmojiUtil.WARN + "选出之后: 5,看个股位置, 打分 1-10");
+
+            System.out.println(EmojiUtil.WARN + "0,看图形, 打分 1-6 " +
+                    "[1-毛刺少 2-庄股走势 3-突破持续性(次日表现) 4-相对位置不过高(防放量滞涨) 5-收敛度高 6-分时量持续] ");
+            System.out.println(EmojiUtil.WARN + "1,看大盘, 打分 -1(下跌趋势) 0(整理) +1(上升 或 即将突破)");
+            System.out.println(EmojiUtil.WARN + "2,看板块, 打分 -1(下降趋势) 0(整理) +1(上升 或 即将突破)");
+            System.out.println(EmojiUtil.WARN + "3,看公告, 打分 -1(利空公告) 0(利好,防高开过度) +1(无公告)");
+            System.out.println(EmojiUtil.WARN + "4,看收益, 打分 -1(最近大涨) 0(近期大亏) +1(平稳)");
+            System.out.println(EmojiUtil.WARN + "选出之后: 总分要求>=8分");
 
         }
     }
