bcfafdc813e962bb75e3408fa5932abe0b23fc54
dwd <weidong.du@opple.com>
2024-01-24 10:46:01 +0800

    upload feishu

--- eastm/scp.sh
+++ eastm/scp.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 scp target/eastm-0.0.1-SNAPSHOT.jar ubuntu@iz01:~/upload
+
 #scp /Users/dwd/dev/GitHub/jiaoyi/tmp/target/tmp-0.0.1-SNAPSHOT.jar ubuntu@iz01:/home/ubuntu/run
 # scp src/main/resources/static/html/line-race.html ubuntu@iz01:~/html/theme.html
 
